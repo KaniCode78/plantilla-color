54aca08603bde12430a452b2746d600e5a1f655a
Kaniltazo <angeldanielrm@icloud.com>
2024-01-27 22:23:23 -0500

    probando

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,20 @@
-document.querySelector('.navbar-item .button').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle('show');
+
+
+
+document.getElementById("boton-hamburguesa").addEventListener("click", function () {
+    var menu = document.getElementById("menu-hamburguesa");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 });
+
+
+document.getElementById("boton-hamburguesa").addEventListener("click", function () {
+    var menu = document.getElementById("menu-hamburguesa");
+    var icono = document.querySelector("boton-hamburguesa span");
+  
+    menu.classList.toggle("mostrando");
+    icono.classList.toggle("rotar");
+  });
